f6d28535d4f357d2886fc061bb05b82ab47f4d79
Taylor Schmidt <taschmid@mines.iedu>
2012-10-11 22:04:59 -0600

    added function + capitalized

--- coolMerge.java
+++ coolMerge.java
@@ -1,10 +1,14 @@
-public class cool {
-	public cool() {
+public class Cool {
+	public Cool() {
 		this.howCool;
 		this.isCool;
 	}
 
 	int howCool;
 	Boolean isCool;
+
+	public void setHowCool(coolness) {
+		this.howCool = coolness;
+	}
 
 }
