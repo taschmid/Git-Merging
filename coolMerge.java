f708422790bbed916f764593325f9b48db30606e
Taylor Schmidt <taschmid@mines.iedu>
2012-10-11 22:08:31 -0600

    deleted isCool

--- coolMerge.java
+++ coolMerge.java
@@ -1,14 +1,13 @@
 public class Cool {
 	public Cool() {
 		this.howCool;
-		this.isCool;
 	}
 
 	int howCool;
-	Boolean isCool;
 
 	public void setHowCool(coolness) {
 		this.howCool = coolness;
+
 	}
 
 }
